79bca23d79cac3ccad770d5d786a91871e920e76
Gabriel Gama <gbr.gama99@gmail.com>
2022-04-12 21:46:49 -0300

    Enum criado para a posição do jogador

---
+++ src/main/java/com/gabfc/register/model/enums/Position.java
@@ -0,0 +1,11 @@
+package com.gabfc.register.model.enums;
+
+public enum Position {
+    STRIKER,
+    ATTACKER_MIDFIELDER,
+    DEFENCIVE_MIDFIELDER,
+    RIGHT_BACK,
+    LEFT_BACK,
+    CENTER_BACK,
+    GOALKEEPER
+}

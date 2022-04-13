a955ce2cdf57bbefa38f98b4c0a19d3366428aa0
Gabriel Gama <gbr.gama99@gmail.com>
2022-04-12 21:49:30 -0300

    Interface Repository para o MS usando SQL

---
+++ src/main/java/com/gabfc/register/repository/PlayerRepository.java
@@ -0,0 +1,9 @@
+package com.gabfc.register.repository;
+
+import com.gabfc.register.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+}
